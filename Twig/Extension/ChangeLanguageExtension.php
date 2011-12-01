1015ecf826e508ab0f839011cb3687fe29bd5cfc
Christian Raue <christian.raue@gmail.com>
2011-12-01 17:04:21 +0100

    removed @Stof as author of ChangeLanguageExtension because `cleanRouteParameters` was dropped with d232de22c70fa6c0fe3a9cc57df06c95283b1480

--- Twig/Extension/ChangeLanguageExtension.php
+++ Twig/Extension/ChangeLanguageExtension.php
@@ -6,7 +6,6 @@ namespace Craue\TwigExtensionsBundle\Twig\Extension;
  * Twig extension providing helpers for implementing a language change mechanism and handling localized routes.
  *
  * @author Christian Raue <christian.raue@gmail.com>
- * @author Christophe Coevoet
  * @copyright 2011 Christian Raue
  * @license http://www.opensource.org/licenses/mit-license.php MIT License
  */
