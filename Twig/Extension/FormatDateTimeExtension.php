1fca277dd29124a576655793230865aa3148c181
Christian Raue <christian.raue@gmail.com>
2012-01-27 18:30:36 +0100

    fixed date/time formatting for DateTime instances with year > 2038 on 32-bit systems

--- Twig/Extension/FormatDateTimeExtension.php
+++ Twig/Extension/FormatDateTimeExtension.php
@@ -149,7 +149,8 @@ class FormatDateTimeExtension extends AbstractLocaleAwareExtension {
 
 		// IntlDateFormatter#format() doesn't support \DateTime objects prior to PHP 5.3.4 (http://php.net/manual/intldateformatter.format.php)
 		if ($valueToUse instanceof \DateTime) {
-			$valueToUse = $valueToUse->getTimestamp();
+			// \DateTime::getTimestamp() would return false for year > 2038 on 32-bit systems
+			$valueToUse = intval($valueToUse->format('U'));
 		}
 
 		$localeToUse = !empty($locale) ? $locale : $this->getLocale();
