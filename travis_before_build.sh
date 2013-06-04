628881100d2ba5380c6deca5d32ff04881c7ab4b
Christian Raue <christian.raue@gmail.com>
2013-06-04 12:17:15 +0200

    use source on Travis avoiding GitHub API limits

--- travis_before_build.sh
+++ travis_before_build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 wget http://getcomposer.org/composer.phar
+
+php composer.phar config -g preferred-install source
+
 sed -i -e "s/\"minimum-stability\": \"dev\"/\"minimum-stability\": \"${MIN_STABILITY}\"/" composer.json
+
 php composer.phar --no-interaction --no-update require symfony/symfony:${SYMFONY_VERSION} symfony/twig-bundle:${SYMFONY_VERSION}
 php composer.phar --no-interaction --no-update require --dev symfony/symfony:${SYMFONY_VERSION}
 php composer.phar --no-interaction update
