71bcc41bb19107eba2cd21f5efd9275eb52edc68
Christian Raue <christian.raue@gmail.com>
2013-09-25 10:07:19 +0200

    adjusted Composer config

--- travis_before_build.sh
+++ travis_before_build.sh
@@ -4,7 +4,7 @@ wget http://getcomposer.org/composer.phar
 
 php composer.phar config -g preferred-install source
 
-sed -i -e "s/\"minimum-stability\": \"dev\"/\"minimum-stability\": \"${MIN_STABILITY}\"/" composer.json
+sed -i -e "s/\"minimum-stability\": \"stable\"/\"minimum-stability\": \"${MIN_STABILITY}\"/" composer.json
 
 php composer.phar --no-interaction --no-update require symfony/symfony:${SYMFONY_VERSION} symfony/twig-bundle:${SYMFONY_VERSION}
 php composer.phar --no-interaction --no-update require --dev symfony/symfony:${SYMFONY_VERSION}
