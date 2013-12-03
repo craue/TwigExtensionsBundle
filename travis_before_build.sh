c3b9e685d5c8513e73c5c366886c7445df982655
Christian Raue <christian.raue@gmail.com>
2013-12-03 17:30:51 +0100

    let Travis run tests with Symfony 2.4.x stable and 2.5.x dev

--- travis_before_build.sh
+++ travis_before_build.sh
@@ -4,8 +4,10 @@ wget http://getcomposer.org/composer.phar
 
 php composer.phar config -g preferred-install source
 
-sed -i -e "s/\"minimum-stability\": \"stable\"/\"minimum-stability\": \"${MIN_STABILITY}\"/" composer.json
+if [ -n "${MIN_STABILITY:-}" ]; then
+	sed -i -e "s/\"minimum-stability\": \"stable\"/\"minimum-stability\": \"${MIN_STABILITY}\"/" composer.json
+fi
 
-php composer.phar --no-interaction --no-update require symfony/symfony:${SYMFONY_VERSION} symfony/twig-bundle:${SYMFONY_VERSION}
-php composer.phar --no-interaction --no-update require --dev symfony/symfony:${SYMFONY_VERSION}
+php composer.phar --no-interaction require --no-update symfony/symfony:${SYMFONY_VERSION} symfony/twig-bundle:${SYMFONY_VERSION}
+php composer.phar --no-interaction require --no-update --dev symfony/symfony:${SYMFONY_VERSION}
 php composer.phar --no-interaction update
