45cc647825d3a745aacdd5455ec6ac18ec706d4b
Christian Raue <christian.raue@gmail.com>
2015-08-18 21:42:34 +0200

    install symfony/phpunit-bridge on Travis when available

--- travis_install_dependencies.sh
+++ travis_install_dependencies.sh
@@ -8,4 +8,9 @@ fi
 
 composer --no-interaction require --no-update symfony/symfony:${SYMFONY_VERSION} symfony/twig-bundle:${SYMFONY_VERSION}
 composer --no-interaction require --no-update --dev symfony/symfony:${SYMFONY_VERSION}
+
+if [ "${PHPUNIT_BRIDGE:-}" = true ]; then
+	composer --no-interaction require --no-update --dev symfony/phpunit-bridge:"${SYMFONY_VERSION}"
+fi
+
 composer --no-interaction update
