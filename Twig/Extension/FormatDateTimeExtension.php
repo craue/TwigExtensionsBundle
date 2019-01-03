9dcab2ec0fd61856bb3fab3dcd0cc0f4d43771a1
Christian Raue <christian.raue@gmail.com>
2019-01-03 12:06:10 +0100

    updated copyright year range

--- Twig/Extension/FormatDateTimeExtension.php
+++ Twig/Extension/FormatDateTimeExtension.php
@@ -9,7 +9,7 @@ use Craue\TwigExtensionsBundle\Util\TwigFeatureUtil;
  * Twig extension providing filters for locale-aware formatting of date, time, and date/time values.
  *
  * @author Christian Raue <christian.raue@gmail.com>
- * @copyright 2011-2018 Christian Raue
+ * @copyright 2011-2019 Christian Raue
  * @license http://opensource.org/licenses/mit-license.php MIT License
  */
 class FormatDateTimeExtension extends AbstractLocaleAwareExtension {
